cd3cfccbab318b86121664045f16b6f44578c9ba
Andrew Max <amax@forever.com>
2014-05-26 23:52:49 -0400

    messing around with design stuff

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
