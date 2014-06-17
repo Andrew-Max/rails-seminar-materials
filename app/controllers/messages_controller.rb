31fc8d76a8d94300c2385b9f4d091f8f09d7596f
Andrew Max <amax@forever.com>
2014-06-16 23:06:52 -0400

    annotates messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,19 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
 
-  # POST /messages
+  # messages#create info
+  # the only messages controller action, it handles the submission of the new message form. If it succesfully creates the message object
+  # based on the parameters from the form it will rerender that users show page with the new comment. Otherwise it will rerender that users
+  # show page with the message and errors (error rendering is not currently functional but does not break the page because the  instance variables are lost in the redirect)
+  # routing info: HTTP POST to => /messages
   def create
     @owner = User.find(message_params[:owner_id])
     @message = Message.new(message_params)
     if @message.save
       redirect_to user_path(@owner), notice: 'Message was successfully created.'
     else
-      render :new
+      @errors = @message.errors
+      redirect_to user_path(@owner)
     end
   end
 
