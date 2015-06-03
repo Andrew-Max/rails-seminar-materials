988492000498029d1d5a5dc5d5ea6417ea024783
andrew <anm898989@gmail.com>
2015-06-02 22:33:21 -0400

    creates challenges in messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,33 +1,36 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
 
-  # messages#create info
-  # the only messages controller action, it handles the submission of the new message form. If it succesfully creates the message object
-  # based on the parameters from the form, it will rerender that user's show page with the new comment. Otherwise it will rerender that users
-  # show page with the message and errors (error rendering is not currently functional but does not break the page because the  instance variables are lost in the redirect)
-  # routing info: HTTP POST to => /messages
-  def index
-    @messages = Message.all
-  end
+  # messages#index notes
+  # routing info: HTTP GET to => /messages
 
+  # An index displays all of a kind of object so the messages#index displays all messages in the database for all users.
+
+  #<<< define a method called index >>>
+  #<<< inside that method create an instance variable @messages and set it equal to all of the messages in the database>>>
+
+  # messages#create notes
+  # routing info: HTTP POST to => /messages
+
+  # This action responds to new message form submissions (the form is on the user#show page)
+  # If it succesfully creates the message object
+  # based on the parameters from the form, it will rerender that user's show page with the new comment.
+  # Otherwise it will rerender that user's show page with an error message
   def create
     @owner = User.find(message_params[:owner_id])
     @message = Message.new(message_params)
     if @message.save
       redirect_to user_path(@owner), notice: 'Message was successfully created.'
     else
-      @errors = @message.errors
-      redirect_to user_path(@owner)
+      redirect_to user_path(@owner), notice: '#{@message.errors[0]}'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_message
       @message = Message.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
       params.require(:message).permit(:body, :author_id, :owner_id)
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
   skip_before_filter :check_session, :except => [:destroy]
 
-  # users#log_out notes
-  # destroys the cookie which stores the current_user id for session faking in app/controllers/application_controller.rb
-  # once this is destroyed there is no current user and it redirects to the home page
-  # routing info: HTTP GET to => /logout
-  def destroy
+  # this code handles how the application knows who the current user is.
+  # it does this by setting and destroy a cookie that holds the id of the current user.
+  # Other controllers can then find the user whos id matches the current_user_id_cookie to figure out who the active user is.
+  # This is simple way to deal with sessions for this sample app but would be a very poor way to do this in a production application.
+
+ def destroy
     cookies.signed[:current_user_id] = nil
     redirect_to new_session_path
   end
