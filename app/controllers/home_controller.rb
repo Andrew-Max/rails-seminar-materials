870672436dcc374576e4c78c973980e2c2a844e6
andrew <anm898989@gmail.com>
2015-06-02 22:41:07 -0400

    creates challenges for home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
-class HomeController < ApplicationController
-  def index
-    @users = User.all.limit(20)
-    @messages = Message.all.limit(100)
-  end
-end
+# Here will be a controller that has a single index action.
+# This will act as the current user's home page
+#<<< create a class with the correct name for a controller for home. It should inherit from Application Controller >>>
+# Look on google for help with ruby inheritance.
+
+#<<< in the class create a single method index >>>
+
+#<<< in the index method create an instance variable @users and set it equal to all users in the database >>>
+#<<< in the index method create an instance variable @messages and set it equal to all messages in the database >>>
