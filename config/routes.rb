e6d4216c635f93bc2dc3f8e2d0ecfbd377f69f97
andrew <anm898989@gmail.com>
2015-06-14 15:46:43 -0400

    updates to routes comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,32 @@ Rails.application.routes.draw do
 
   #General Comments
   #################################################################################
-  # Get an overview of all your route at the command line with "$rake routes".
+  # NOTE: Get an overview of all your route at the command line with "$rake routes".
 
   # There are two basic ways to declare the routes for your application:
   # individually or as a set of actions for a resource.
 
   # To declare a route individually you must specify an http action, a url name, and an action in a controller
+
   # the format is:
-  # <verb> '<string url>', to: '<controlle>#<action>'
+  # <verb> '<string url>', to: '<controller>#<action>'
+
   # an example would be
   # get '/photo', to: 'photo#index'
 
   # The second way is to declare all of the routes for a resource
+
   # an example would be
   # resources :photo
+
   # be default this will create 7 routes based around the user resource: index, show, edit, update, new, create, destroy
+
   # you can also limit a resource if you don't need all the routes. an example would be:
-  # resources :photos, only: [:show, :sdestroy]
+
+  # resources :photos, only: [:show, :destroy]
   # or
   # resources :photos, except: [:show, :destroy]
+
   # for more info on resources checkout http://guides.rubyonrails.org/routing.html#crud-verbs-and-actions
 
   # there is also usually a special route declaration that specifies where the base url '/' routes to
