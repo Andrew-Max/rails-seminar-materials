3381d679847431d26d8a087df66302bb6a7b3610
andrew <anm898989@gmail.com>
2015-06-14 16:08:27 -0400

    improves message model comments

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,13 @@
-# this is message model. It defines the basic properties of a messsage object. Note that
-# does not create the messages table in the database, that is done through migration.
-# This model simply defines a message's relationship to a user. However because a message
-# has an author and an owner we have to define its relationship to user twice. Becaause
+# This is message model. It defines the basic properties of a messsage object.
+
+# Note that does not create the messages table in the database, that is done through migration.
+
+# This model defines a message's relationship to a user. However because a message
+# has an author and an owner we have to define its relationship to user twice. Because
 # we are saying messages belong to things which arent classes, we have to tell them what class
-# these things are so ActiveRecord know how to query for these things.
-# This model validates the messages have a body. Messages without a body will not be able
+# these things are so ActiveRecord know how to query for them.
+
+# This model validates that messages have a body. Messages without a body will not be able
 # to be saved
 class Message < ActiveRecord::Base
 
